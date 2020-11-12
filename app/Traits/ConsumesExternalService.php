6085c374e23bd1eb4a66adf25ad8850190dc92a0
junior <jjquelito@gmail.com>
2020-11-12 16:55:01 -0300

    APIP Gateway autenticando as requisições

--- app/Traits/ConsumesExternalService.php
+++ app/Traits/ConsumesExternalService.php
@@ -16,8 +16,8 @@ trait ConsumesExternalService
             'base_uri' => $this->baseUri,
         ]);
 
-        if(isset($this->secret)){
-            $headers['Authorization'] => $this->secret;
+        if (isset($this->secret)) {
+            $headers['Authorization'] = $this->secret;
         }
 
         $response = $client->request($method, $requestUrl, ['form_params' => $formParams, 'headers' => $headers]);
